42ebe64488b1e7edfd9bdbb5bc57cb529989c1d9
Callupe72 <paulleduc72@gmail.com>
2022-06-03 13:56:40 +0200

    Working on animator custom

--- Assets/Editor/AnimatorCustomWindow.cs
+++ Assets/Editor/AnimatorCustomWindow.cs
@@ -1,21 +1,40 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.Tilemaps;
 
+
+[ExecuteInEditMode]
 public class AnimatorCustomWindow : EditorWindow
 {
+    ListOfAllAnimatorWindowCustomEditor listWindow;
+
+    //Setup
+    public Color selectedColor = Color.blue;
+    private float _lastEditorTime = 0f;
+    bool autoRefresh = false;
+
+    //Create animator
     string animatorName = "New Animator";
     public RuntimeAnimatorController newController;
-    List<GameObject> selectedObj = new List<GameObject>();
-    string[] texuresString = { "Play", "Pause"};
+
+
+    public List<GameObject> selectedObj = new List<GameObject>();
     bool searchWithFindObjectOfType = true;
-    Color selectedColor = Color.red;
+    List<string> textureString = new List<string>();
 
-    float time = 0;
+    public List<Animator> anims = new List<Animator>();
 
-    AnimationClip currentClip = null;
+    public AnimationClip currentClip = null;
+    bool animationCanLoop;
+    bool animationIsPlaying;
+
+    float animSpeed = 1;
+    float animTime;
+    float waitTimeBetween2Loop = 0;
+    bool isWaitingForDelayUpdate;
 
     // Add menu named "My Window" to the Window menu
     [MenuItem("Paul/Animator Window")]
@@ -26,6 +45,75 @@ public class AnimatorCustomWindow : EditorWindow
         window.Show();
     }
 
+    async void DelayUseAsync()
+    {
+        isWaitingForDelayUpdate = true;
+        await Task.Delay(Mathf.RoundToInt(waitTimeBetween2Loop * 1000));
+        isWaitingForDelayUpdate = false;
+        if (animationIsPlaying && AnimationMode.InAnimationMode())
+        {
+            StartAnimation();
+        }
+    }
+
+    void CreateButtonsList()
+    {
+        textureString.Clear();
+        textureString.Add("Play");
+        textureString.Add("Pause");
+        textureString.Add("Loop");
+        textureString.Add("Stop");
+    }
+
+    void RefreshAnimatorInScene()
+    {
+        anims.Clear();
+        if (searchWithFindObjectOfType)
+        {
+            foreach (var item in FindObjectsOfType<Animator>())
+            {
+                anims.Add(item);
+            }
+        }
+        else
+        {
+            foreach (var item in SceneManager.GetActiveScene().GetRootGameObjects())
+            {
+                foreach (Transform child in item.transform)
+                {
+                    if (child.GetComponent<Animator>())
+                    {
+                        anims.Add(child.GetComponent<Animator>());
+                    }
+                }
+                if (item.GetComponent<Animator>())
+                {
+                    anims.Add(item.GetComponent<Animator>());
+                }
+            }
+        }
+    }
+
+    void OpenNewWindowForList()
+    {
+        GUI.backgroundColor = listWindow != null ? selectedColor : Color.white;
+        if (GUILayout.Button("See list in another window"))
+        {
+            if (listWindow == null)
+            {
+                listWindow = GetWindow<ListOfAllAnimatorWindowCustomEditor>();
+                listWindow.animatorCustomWindow = this;
+                listWindow.Show();
+            }
+            else
+            {
+                listWindow.Close();
+                listWindow = null;
+            }
+        }
+        GUI.backgroundColor = Color.white;
+    }
+
     void OnGUI()
     {
         if (Application.isPlaying)
@@ -34,102 +122,208 @@ public class AnimatorCustomWindow : EditorWindow
             return;
         }
 
-        EditorGUILayout.LabelField("Setup");
+        //GUILayout.BeginScrollView(Vector2.zero);
 
+        CreateCategory("Setup", true);
         selectedColor = EditorGUILayout.ColorField(selectedColor);
 
-        EditorGUILayout.LabelField("All animators in scene");
+        GUI.backgroundColor = autoRefresh ? selectedColor : Color.white;
+
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("Auto Refresh"))
+        {
+            autoRefresh = !autoRefresh;
+        }
+
+        GUI.backgroundColor = Color.white;
+
+        if (!autoRefresh)
+        {
+            if (GUILayout.Button("Refresh"))
+            {
+                RefreshAnimatorInScene();
+            }
+        }
+        else
+        {
+            RefreshAnimatorInScene();
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        OpenNewWindowForList();
 
         searchWithFindObjectOfType = EditorGUILayout.Toggle("Search with FindObjectOfType", searchWithFindObjectOfType);
 
-        Animator[] anims = GetAllAnimatorsInScene();
+        EditorGUILayout.EndVertical();
 
         GUIStyle style = new GUIStyle(GUI.skin.button);
 
         style.alignment = TextAnchor.MiddleCenter;
 
-        EditorGUILayout.BeginHorizontal();
-        SelectAll(anims);
-        UnSelectAll();
-        EditorGUILayout.EndHorizontal();
 
-        for (int i = anims.Length - 1; i >= 0; --i)
+        if (anims.Count > 0 && listWindow == null)
         {
-            Animator currentAnim = anims[i];
-            bool isSelected = Selection.Contains(currentAnim.gameObject);
+            CreateCategory("Select All animations", false);
+            SelectAllButton();
+            UnSelectAllButton();
+            EditorGUILayout.EndHorizontal();
 
-            GUI.backgroundColor = isSelected ? selectedColor : Color.white;
-            style.fontStyle = isSelected ? FontStyle.Italic : FontStyle.Bold;
-
-            EditorGUILayout.Space(10);
-
-            ButtonSelectAnim(currentAnim, style, isSelected);
-            if (isSelected)
+            for (int i = anims.Count - 1; i >= 0; --i)
             {
-                SeeAllAnimsOfAnimController(currentAnim);
+                Animator currentAnim = anims[i];
+                bool isSelected = Selection.Contains(currentAnim.gameObject);
+
+                GUI.backgroundColor = isSelected ? selectedColor : Color.white;
+                style.fontStyle = isSelected ? FontStyle.Italic : FontStyle.Bold;
+
+                EditorGUILayout.Space(10);
+
+                ButtonSelectAnim(currentAnim, style, isSelected);
+                if (isSelected)
+                {
+                    SeeAllAnimsOfAnimController(currentAnim);
+                }
+                GUI.backgroundColor = Color.white;
             }
-            GUI.backgroundColor = Color.white;
+
+            EditorGUILayout.Space(20);
         }
 
-        EditorGUILayout.Space(20);
-
         PlayAnimation();
-
         CreateNewAnimator();
     }
 
     #region Select
-    void SelectAll(Animator[] anims)
+    void SelectAllButton()
     {
         if (GUILayout.Button("Select All"))
         {
-            selectedObj.Clear();
-            foreach (var item in anims)
-            {
-                selectedObj.Add(item.gameObject);
-            }
-            ActualizeSelection();
+            SelectAll();
         }
     }
-    void UnSelectAll()
+    void UnSelectAllButton()
     {
         if (GUILayout.Button("Unselect All"))
         {
-            selectedObj.Clear();
-            ActualizeSelection();
-            ChangeCurrentClip(null);
+            UnSelectAll();
         }
+    }
+
+    public void SelectAll()
+    {
+        selectedObj.Clear();
+        foreach (var item in anims)
+        {
+            selectedObj.Add(item.gameObject);
+        }
+        ActualizeSelection();
+    }
+
+    public void UnSelectAll()
+    {
+        selectedObj.Clear();
+        ActualizeSelection();
+        ChangeCurrentClip(null);
     }
     #endregion
 
-    Animator[] GetAllAnimatorsInScene()
+    void FakeUpdate()
     {
-        if (searchWithFindObjectOfType)
+        if (currentClip == null)
+            return;
+
+        if (isWaitingForDelayUpdate)
+            return;
+
+        if (animationIsPlaying)
         {
-            return FindObjectsOfType<Animator>();
-        }
-        else
-        {
-            List<Animator> animators = new List<Animator>();
-            foreach (var item in SceneManager.GetActiveScene().GetRootGameObjects())
+            animTime = (Time.realtimeSinceStartup - _lastEditorTime) * animSpeed;
+            if (animTime >= currentClip.length)
             {
-                foreach (Transform child in item.transform)
+                if (!animationCanLoop)
                 {
-                    if (child.GetComponent<Animator>())
+                    StopAnimation();
+                }
+                else
+                {
+                    _lastEditorTime = Time.realtimeSinceStartup;
+                    if (waitTimeBetween2Loop > 0)
                     {
-                        animators.Add(child.GetComponent<Animator>());
+                        DelayUseAsync();
                     }
                 }
-                if (item.GetComponent<Animator>())
+            }
+            else
+            {
+                if (AnimationMode.InAnimationMode())
                 {
-                    animators.Add(item.GetComponent<Animator>());
+                    foreach (var go in Selection.gameObjects)
+                    {
+                        if (go.GetComponent<Animator>())
+                        {
+                            AnimationMode.SampleAnimationClip(go, currentClip, animTime);
+                        }
+                    }
                 }
             }
-            return animators.ToArray();
+        }
+
+        Repaint();
+    }
+
+    #region Enable/Disable Scene
+
+    void OnEnable()
+    {
+        CreateButtonsList();
+        EditorApplication.playModeStateChanged += _OnPlayModeStateChange;
+        EditorSceneManager.sceneOpened += _ChangeScene;
+    }
+    private void OnDisable()
+    {
+        EditorApplication.playModeStateChanged -= _OnPlayModeStateChange;
+        EditorSceneManager.sceneOpened -= _ChangeScene;
+        StopAnimation();
+    }
+
+    #endregion
+
+    void _ChangeScene(Scene currentScene, OpenSceneMode sceneMod)
+    {
+        currentClip = null;
+        StopAnimation();
+        RefreshAnimatorInScene();
+
+    }
+
+    void _OnPlayModeStateChange(PlayModeStateChange state)
+    {
+        if (state == PlayModeStateChange.ExitingEditMode)
+        {
+            StopAnimation();
         }
     }
 
-    void ActualizeSelection()
+    void StartAnimation()
+    {
+        AnimationMode.StartAnimationMode();
+        EditorApplication.update -= FakeUpdate;
+        EditorApplication.update += FakeUpdate;
+        _lastEditorTime = Time.realtimeSinceStartup;
+        isWaitingForDelayUpdate = false;
+        animationIsPlaying = true;
+    }
+
+    void StopAnimation()
+    {
+        animationIsPlaying = false;
+        AnimationMode.StopAnimationMode();
+        EditorApplication.update -= FakeUpdate;
+    }
+
+    public void ActualizeSelection()
     {
         //Je fais ça pour ne pas que la sélection se fasse par dessus la selection par la hierarchy
         Selection.objects = selectedObj.ToArray();
@@ -141,9 +335,10 @@ public class AnimatorCustomWindow : EditorWindow
 
     void CreateNewAnimator()
     {
-        EditorGUILayout.HelpBox("Create Animators", MessageType.Info);
-        animatorName = EditorGUILayout.TextField("Object Name: ", animatorName);
-        newController = EditorGUILayout.ObjectField(newController, typeof(RuntimeAnimatorController), true) as RuntimeAnimatorController;
+        EditorGUILayout.Space(20);
+        CreateCategory("Create new Animator", true);
+        animatorName = EditorGUILayout.TextField("Object Name : ", animatorName);
+        newController = EditorGUILayout.ObjectField("Object Controller : ", newController, typeof(RuntimeAnimatorController), true) as RuntimeAnimatorController;
         if (GUILayout.Button("Add Animator"))
         {
             GameObject animatorGO = new GameObject();
@@ -151,7 +346,26 @@ public class AnimatorCustomWindow : EditorWindow
             anim.runtimeAnimatorController = newController;
             animatorGO.name = animatorName;
         }
+
+        EditorGUILayout.EndVertical();
+
     }
+
+    void CreateCategory(string titleName, bool isVertical)
+    {
+        EditorGUILayout.Space(10);
+        if (isVertical)
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+        }
+        else
+        {
+            EditorGUILayout.BeginHorizontal(EditorStyles.helpBox);
+        }
+        EditorGUILayout.LabelField(titleName, EditorStyles.boldLabel);
+        EditorGUILayout.Space(7);
+    }
+
     void ButtonSelectAnim(Animator currentAnim, GUIStyle style, bool isSelected)
     {
         if (GUILayout.Button(currentAnim.name))
@@ -194,42 +408,132 @@ public class AnimatorCustomWindow : EditorWindow
 
     void PlayAnimation()
     {
-        if (currentClip != null)
+        if (selectedObj.Count > 0)
         {
-            float startTime = 0.0f;
-            float stopTime = currentClip.length;
-            AnimationMode.BeginSampling();
-            AnimationMode.EndSampling();
-            time = EditorGUILayout.Slider(time, startTime, stopTime);
-            foreach (GameObject item in Selection.objects)
+            if (currentClip != null)
             {
-                AnimationMode.SampleAnimationClip(item, currentClip, time);
-            }
+                AnimationMode.StartAnimationMode();
 
-            EditorGUILayout.BeginHorizontal();
-
-            for (int i = 0; i < 2; i++)
-            {
-                Texture2D play = EditorGUIUtility.Load("Assets/Editor/Icon/"+ texuresString[i] + ".png") as Texture2D;
-                Rect rect = EditorGUILayout.GetControlRect();
-                if (GUI.Button(rect, play))
+                CreateCategory("Play Animations", true);
+                CreateAnimationButtons();
+                EditorGUILayout.EndHorizontal();
+                EditorGUILayout.BeginVertical();
+                animSpeed = EditorGUILayout.Slider("Anim Speed", animSpeed, 0f, 10f);
+                EditorGUI.BeginChangeCheck();
+                EditorGUILayout.BeginHorizontal();
+                animTime = EditorGUILayout.Slider("Anim Time", animTime, 0, currentClip.length);
+                string clipName = currentClip != null ? currentClip.name : "";
+                EditorGUILayout.LabelField(" / " + (currentClip.length).ToString("F3") + " | " + Mathf.RoundToInt((animTime / currentClip.length) * 100) + " % " + " | " + clipName);
+                if (EditorGUI.EndChangeCheck())
                 {
-
+                    StopAnimation();
+                    foreach (var go in Selection.gameObjects)
+                    {
+                        if (go.GetComponent<Animator>())
+                        {
+                            AnimationMode.StartAnimationMode();
+                            AnimationMode.SampleAnimationClip(go, currentClip, animTime);
+                        }
+                    }
                 }
+                EditorGUILayout.EndHorizontal();
+                if (!animationIsPlaying)
+                {
+                    waitTimeBetween2Loop = EditorGUILayout.Slider("Time between 2 loop", waitTimeBetween2Loop, 0f, 5f);
+                }
+                EditorGUILayout.EndVertical();
             }
-            EditorGUILayout.EndHorizontal();
-        }
-    }
-
-    void ChangeCurrentClip(AnimationClip newClip)
-    {
-        if (currentClip == newClip)
-        {
-            currentClip = null;
+            else
+            {
+                EditorGUILayout.HelpBox("Select an animation to use it", MessageType.Info);
+            }
         }
         else
         {
-            currentClip = newClip;
+            EditorGUILayout.HelpBox("Select a game Object with animator to use it", MessageType.Info);
         }
+    }
+
+    public void AddInSelection(GameObject objectToAdd)
+    {
+        if (!selectedObj.Contains(objectToAdd))
+        {
+            selectedObj.Add(objectToAdd);
+            ActualizeSelection();
+        }
+    }
+    public void RemoveFromSelection(GameObject objectToAdd)
+    {
+        if (selectedObj.Contains(objectToAdd))
+        {
+            selectedObj.Remove(objectToAdd);
+            ActualizeSelection();
+        }
+    }
+
+    void CreateAnimationButtons()
+    {
+        AnimationPlayButton();
+        AnimationPauseButton();
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.BeginHorizontal();
+        AnimationLoopButton();
+        AnimationStopButton();
+    }
+
+    void AnimationPlayButton()
+    {
+        Texture2D texture = EditorGUIUtility.Load("Assets/Editor/Icon/ButtonsControlAnimations/" + textureString[0] + ".png") as Texture2D;
+        Rect rect = EditorGUILayout.GetControlRect();
+
+        GUI.backgroundColor = animationIsPlaying ? selectedColor : Color.white;
+
+        if (GUI.Button(rect, texture))
+        {
+            StartAnimation();
+        }
+
+        GUI.backgroundColor = Color.white;
+    }
+    void AnimationPauseButton()
+    {
+        Texture2D texture = EditorGUIUtility.Load("Assets/Editor/Icon/ButtonsControlAnimations/" + textureString[1] + ".png") as Texture2D;
+        Rect rect = EditorGUILayout.GetControlRect();
+
+        GUI.backgroundColor = !animationIsPlaying ? selectedColor : Color.white;
+        if (GUI.Button(rect, texture))
+        {
+            StopAnimation();
+        }
+        GUI.backgroundColor = Color.white;
+    }
+    void AnimationLoopButton()
+    {
+        Texture2D texture = EditorGUIUtility.Load("Assets/Editor/Icon/ButtonsControlAnimations/" + textureString[2] + ".png") as Texture2D;
+        Rect rect = EditorGUILayout.GetControlRect();
+
+        GUI.backgroundColor = animationCanLoop ? selectedColor : Color.white;
+        if (GUI.Button(rect, texture))
+        {
+            animationCanLoop = !animationCanLoop;
+        }
+
+        GUI.backgroundColor = Color.white;
+    }
+    void AnimationStopButton()
+    {
+        Texture2D texture = EditorGUIUtility.Load("Assets/Editor/Icon/ButtonsControlAnimations/" + textureString[3] + ".png") as Texture2D;
+        Rect rect = EditorGUILayout.GetControlRect();
+
+        if (GUI.Button(rect, texture))
+        {
+            animTime = 0;
+            StopAnimation();
+        }
+    }
+
+    public void ChangeCurrentClip(AnimationClip newClip)
+    {
+        currentClip = currentClip == newClip ? null : newClip;
     }
 }
---
+++ Assets/Editor/ListOfAllAnimatorWindowCustomEditor.cs
@@ -0,0 +1,141 @@
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class ListOfAllAnimatorWindowCustomEditor : EditorWindow
+{
+    public AnimatorCustomWindow animatorCustomWindow;
+
+    string searchString = "";
+
+    void SeeAllAnimators()
+    {
+        if (GUILayout.Button("Select All"))
+        {
+            animatorCustomWindow.SelectAll();
+        }
+        if (GUILayout.Button("Unselect All"))
+        {
+            animatorCustomWindow.UnSelectAll();
+        }
+    }
+
+    void RemoveFromSearch()
+    {
+        searchString = "";
+        GUI.FocusControl(null);
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.BeginHorizontal(GUI.skin.FindStyle("Toolbar"));
+        GUI.backgroundColor = Color.green;
+        if (GUILayout.Button("V", GUILayout.Width(50)))
+        {
+            if (searchString != "")
+            {
+                foreach (var item in animatorCustomWindow.anims)
+                {
+                    if (item.name.ToLower().Contains(searchString))
+                    {
+                        animatorCustomWindow.AddInSelection(item.gameObject);
+                    }
+                    else
+                    {
+                        animatorCustomWindow.RemoveFromSelection(item.gameObject);
+                    }
+                }
+            }
+        }
+        GUI.backgroundColor = Color.white;
+        searchString = GUILayout.TextField(searchString, GUI.skin.FindStyle("ToolbarSeachTextField"));
+        GUI.backgroundColor = Color.red;
+        if (GUILayout.Button("X", GUILayout.Width(50)))
+        {
+            RemoveFromSearch();
+        }
+        GUI.backgroundColor = Color.white;
+
+
+        //EditorGUILayout.BeginHorizontal();
+        //EditorGUILayout.LabelField("Selected item", GUILayout.ExpandWidth(false), GUILayout.Width(250));
+
+        //if (GUILayout.Button(searchString, EditorStyles.popup))
+        //{
+        //    SearchWindow.Open(new SearchWindowContext(GUIUtility.GUIToScreenPoint(Event.current.mousePosition)), new StringListSearchProvider());
+        //}
+        GUILayout.EndHorizontal();
+
+        SeeAllAnimators();
+        if (animatorCustomWindow.anims.Count > 0)
+        {
+            GUIStyle style = new GUIStyle(GUI.skin.button);
+
+            style.alignment = TextAnchor.MiddleCenter;
+
+            for (int i = animatorCustomWindow.anims.Count - 1; i >= 0; --i)
+            {
+                Animator currentAnim = animatorCustomWindow.anims[i];
+                bool isSelected = Selection.Contains(currentAnim.gameObject);
+
+                GUI.backgroundColor = isSelected ? animatorCustomWindow.selectedColor : Color.white;
+                style.fontStyle = isSelected ? FontStyle.Italic : FontStyle.Bold;
+
+                EditorGUILayout.Space(10);
+
+                ButtonSelectAnim(currentAnim, style, isSelected);
+                if (isSelected)
+                {
+                    SeeAllAnimsOfAnimController(currentAnim);
+                }
+                GUI.backgroundColor = Color.white;
+            }
+        }
+
+
+
+
+        EditorGUILayout.Space(20);
+    }
+
+    void SeeAllAnimsOfAnimController(Animator currentAnim)
+    {
+        if (currentAnim.runtimeAnimatorController == null)
+        {
+            EditorGUILayout.HelpBox("There is no controller", MessageType.Error);
+            return;
+        }
+        EditorGUILayout.Space(5);
+        EditorGUILayout.BeginHorizontal();
+
+        foreach (AnimationClip ac in currentAnim.runtimeAnimatorController.animationClips)
+        {
+            GUI.backgroundColor = ac == animatorCustomWindow.currentClip ? animatorCustomWindow.selectedColor : Color.white;
+
+            if (GUILayout.Button(ac.name))
+            {
+                animatorCustomWindow.ChangeCurrentClip(ac);
+            }
+            GUI.backgroundColor = Color.white;
+        }
+        EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space(20);
+    }
+
+
+    void ButtonSelectAnim(Animator currentAnim, GUIStyle style, bool isSelected)
+    {
+        if (GUILayout.Button(currentAnim.name))
+        {
+            if (isSelected)
+            {
+                animatorCustomWindow.selectedObj.Remove(currentAnim.gameObject);
+            }
+            else
+            {
+                animatorCustomWindow.selectedObj.Add(currentAnim.gameObject);
+            }
+            animatorCustomWindow.ActualizeSelection();
+        }
+    }
+}
